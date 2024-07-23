2d51b6e04cf763b2e3b0e7b80de9d73863b162b7
Giulio Montaque <giuliom@hotmail.co.uk>
2024-07-23 11:25:33 +0000

    update

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+
+axios.defaults.baseURL = 'https://rest-framework-app-aed304802b04.herokuapp.com/'
+axios.defaults.headers.post['Content-type'] = 'mulipart/form-data'
+axios.defaults.withCredentials = true
